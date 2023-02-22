3aae05ba521523ac81809581876a402fc42df732
Rian Barbosa <rianbbarbosa@outlook.com>
2023-02-22 11:27:20 -0300

    Alright!

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 // middleware
 app.use(express.json());
 app.use('/movies/', moviesRouter);
-
+app.use('/api-docs', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 app.use('/.netlify/functions/server', router);
 module.exports = app;
 module.exports.handler = serverless(app);
