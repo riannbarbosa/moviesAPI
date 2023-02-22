71f113465ae80cbde482bb853bcddb572666f52f
Rian Barbosa <rianbbarbosa@outlook.com>
2023-02-22 11:28:47 -0300

    Alright!

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+const path = require('path');
 import swaggerUI from 'swagger-ui-express'
 import cors from 'cors'
 import serverless from 'serverless-http'
@@ -18,7 +19,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 // middleware
 app.use(express.json());
 app.use('/movies/', moviesRouter);
-app.use('/api-docs', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/api-docs', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 app.use('/.netlify/functions/server', router);
 module.exports = app;
 module.exports.handler = serverless(app);
