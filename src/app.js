0e63fa86b8ca4af98043806e68b6d88f8d98538e
Rian Barbosa <rianbbarbosa@outlook.com>
2023-02-23 09:20:02 -0300

    Swagger UI config

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
 import express from 'express';
-import swaggerUI from 'swagger-ui-express'
+/* import swaggerUI from 'swagger-ui-express' */
+import path from 'path'
 import cors from 'cors'
 
 const app = express();
 
 const moviesRouter = require('./routes/moviesList');
 
-import swaggerDocs from './swagger.json';
-
-app.use(cors());
-app.options('*', cors());
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+/* import swaggerDocs from './swagger.json'; */
 
 // middleware
 app.use(express.json());
 app.use('/movies/', moviesRouter);
 
-const options = {
+app.use(cors());
+app.options('*', cors());
+
+/* const options = {
     customCss: '.swagger-ui .topbar { display: none }'
 }
+ */
+app.get('/api-docs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'swagger-ui.html'));
+  });
+  
+/* app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs, options)); */
+
+
 
 // exporting swagger and app to deployment
 
 module.exports = app;
-module.exports = swaggerUI.setup(swaggerDocs, options);
