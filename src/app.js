554b4af0d25fb3afca53ef759f76f3fbb602e9d1
Rian Barbosa <rianbbarbosa@outlook.com>
2023-02-23 08:44:47 -0300

    Vercel Configs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import express from 'express';
-const path = require('path');
 import swaggerUI from 'swagger-ui-express'
 import cors from 'cors'
-import serverless from 'serverless-http'
-const router = express.Router();x
 
 const app = express();
 
@@ -19,8 +16,5 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 // middleware
 app.use(express.json());
 app.use('/movies/', moviesRouter);
-app.use('/api-docs', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
-app.use('/.netlify/functions/server', router);
 module.exports = app;
-module.exports.handler = serverless(app);
  
